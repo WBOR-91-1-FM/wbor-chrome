4e6f381a117c559a1a1272221e33eb6f1b9383cb
Ruben Martinez Jr. <rmartin@bowdoin.edu>
2014-02-11 23:41:20 -0500

    Bugfix. Same thing as last time, but got it now.

--- message.js
+++ message.js
@@ -1,24 +1,31 @@
+var NP = false;
 chrome.extension.sendMessage({action: "get_status"}, function(response) {
   if(response.status) {
       $('#start').css('background-image', 'url(play2.png)');
       $('#stop').css('background-image', 'url(pause.png)');
+      NP = true;
       nowPlaying();
   }
   else {
       $('#stop').css('background-image', 'url(pause2.png)');
       $('#play').css('background-image', 'url(play.png)');
       $('#info').html('');
+      NP = false;
   }
 });
 $('#start').click(function(){
     chrome.extension.sendMessage({action: "play"});
     $('#start').css('background-image', 'url(play2.png)');
     $('#stop').css('background-image', 'url(pause.png)');
+    NP = true;
+    nowPlaying();
 });
 $('#stop').click(function(){
     chrome.extension.sendMessage({action: "stop"});
     $('#start').css('background-image', 'url(play.png)');
     $('#stop').css('background-image', 'url(pause2.png)');
+    $('#info').html('');
+    NP = false;
 });
 function nowPlaying() {
     $('#info').html('<div class=spinner>B</div>');
@@ -27,5 +34,5 @@ function nowPlaying() {
         $('#song').html(data.song_string);
         $('#artist').html(data.artist_string);
     });
-    setTimeout(function(){nowPlaying()}, 60000);
+    if(NP) setTimeout(function(){nowPlaying()}, 60000);
 }
